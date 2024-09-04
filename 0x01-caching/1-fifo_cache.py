3e6a1aad01067ca9dd10ce2e58710d612ba30ad9
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-04 16:51:37 +0200

    added comments to get and put methods

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,14 +5,31 @@ from base_caching import BaseCaching
 BasicCache = __import__("0-basic_cache").BasicCache
 
 class FIFOCache(BaseCaching):
-    """  inherits from BaseCaching and is a caching system """
+    """  Implements a FIFO caching system """
 
     def put(self, key, item):
+        """
+        overides `put` from BasicCache class,
+        deletes the first inserted item from the dictionary `self.cache_data`
+        and prints the removed item's key.
+        Args:
+            key (str)
+            item -(can be of any type)
+        Return:
+            Void.
+        """
         BasicCache.put(self, key, item)
-        max_items = BaseCaching.MAX_ITEMS
+        max_items = super().MAX_ITEMS
         if len(self.cache_data) > max_items:
             first_key = next(iter(self.cache_data))
             del self.cache_data[first_key]
             print("DISCARD: {}".format(first_key))
     def get(self, key):
+        """
+        retrieves a value  linked to `key` from self.cache_data dictionary.
+        Args:
+            key (str) - dictionary key
+        Return:
+            value associated with the key if the key exists, None otherwise.
+        """
         return BasicCache.get(self, key)
