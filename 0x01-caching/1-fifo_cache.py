554be0d8812cc7635675afbd6fd63487ea234523
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-04 14:42:33 +0200

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """Comprises class FIFOCache """
 
+from base_caching import BaseCaching
 BasicCache = __import__('0-basic_cache').BasicCache
 
-class FIFOCache(BasicCache):
+class FIFOCache(BaseCaching):
     """  inherits from BaseCaching and is a caching system """
+    def __init__(self):
+        super().__init__()
+
     def put(self, key, item):
-        super().put(key, item)
+        BasicCache.put(self, key, item)
         max_items = super().MAX_ITEMS
         if len(self.cache_data) > max_items:
             first_key = next(iter(self.cache_data))
@@ -14,4 +18,4 @@ class FIFOCache(BasicCache):
             print(f"DISCARD: {first_key}")
 
     def get(self, key):
-        return super().get(key)
+        return BasicCache.get(self, key)
