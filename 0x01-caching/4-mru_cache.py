decb0f7b543558048c00ac5522a4ded14b58777f
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-05 19:49:38 +0200

    fixed a typo and implemented mru algorithm

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" LRU caching system """
+""" MRU caching system """
 from base_caching import BaseCaching
+LRUCache = __import__("3-lru_cache").LRUCache
 
 
-class LRUCache(BaseCaching):
-    """ Implements Least Recent Used (LRU) caching algorithm """
+class MRUCache(BaseCaching):
+    """ Implements Most Recent Used (LRU) caching algorithm """
     def __init__(self):
         super().__init__()
         self.key_order = []
@@ -12,11 +13,10 @@ class LRUCache(BaseCaching):
     def put(self, key, item):
         """
         Add or update an item in the cache.
-        
         - If the key already exists, update its value
           and move it to the end of the list.
         - If the key does not exist and the cache is full,
-            remove the least recently used item (first item in the list).
+            remove the most recently used item (last item in the list).
         - Add the new item to the end of the list.
         """
         if key is None or item is None:
@@ -24,23 +24,17 @@ class LRUCache(BaseCaching):
         if key in self.cache_data:
             self.key_order.remove(key)
         elif len(self.cache_data) >= self.MAX_ITEMS:
-            lru_key = self.key_order.pop(0)
-            del self.cache_data[lru_key]
-            print(f"DISCARD: {lru_key}")
+            mru_key = self.key_order.pop()
+            del self.cache_data[mru_key]
+            print(f"DISCARD: {mru_key}")
         self.cache_data[key] = item
         self.key_order.append(key)
 
     def get(self, key):
         """
         Retrieve an item from the cache.
-        
         - If the key exists, move it to the end of the list
             (mark it as most recently used) and return its value.
         - If the key does not exist, return None.
         """
-        value = self.cache_data.get(key)
-        if value is not None:
-            self.key_order.remove(key)
-            self.key_order.append(key)
-            return value
-        return None
+        return LRUCache.get(self, key)
