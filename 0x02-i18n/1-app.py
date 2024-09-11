d1008ee9a9c08292f0352c4a094f26405efbf2c4
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-11 04:11:11 +0200

    1. Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 """ instamtiate Babel object"""
-from flask import request
+from config import Config
 from flask_babel import Babel
 app = __import__("0-app").app
 
 
-class Config:
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+app.config.from_object(Config)
 
-def get_locale:
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-babel = Babel(app, local_selector=get_locale)
+babel = Babel(app)
