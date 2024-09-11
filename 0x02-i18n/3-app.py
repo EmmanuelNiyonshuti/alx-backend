f340838046595c37479e5cff9fa8de090d25afff
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-11 13:48:47 +0200

    comments

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
-""" Get locale from requests implementation"""
-from flask import request
-app = __import__("0-app").app
-babel = __import__("1-app").babel
+""" Parametrize templates """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+Config = __import__("1-app").Config
 
+app = Flask(__name__)
 
+app.config.from_object(Config)
+
+# Using @babel.localeselector is deprecated, but required for this project:
 @babel.localeselector
 def get_locale():
     """ get_locale function """
+    # return "fr"
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+# This is the newer approach, but not used here
+# because locale_selector is required:
+# babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route("/")
+def home():
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
